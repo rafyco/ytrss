4b7ff4aa97622030c82bdff234fd40a8c6d5b51a
Rafał Kobel <rafalkobel@rafyco.pl>
2022-03-28 02:10:06 +0200

    Python template package initialization

---
+++ README.rst
@@ -0,0 +1,76 @@
+..
+    TODO: change package name
+
+=======================================
+template-python-package
+=======================================
+
+Program to automatic download YouTube files by ``youtube_dl`` scripts.
+
+.. image:: https://img.shields.io/badge/author-Rafa%C5%82%20Kobel-blue.svg
+    :target: https://rafyco.pl
+
+..
+    TODO: change package name
+
+    .. image:: https://github.com/rafyco/template-ptyhon-package/actions/workflows/pythonpackage.yml/badge.svg?branch=master
+       :target: https://github.com/rafyco/template-python-package/actions/workflows/pythonpackage.yml
+
+    .. image:: https://img.shields.io/readthedocs/template-python-package.svg
+       :target: https://template-python-package.readthedocs.io
+
+.. image:: https://img.shields.io/github/last-commit/rafyco/template-python-package.svg
+   :target: https://github.com/rafyco/template-python-package
+
+.. image:: https://img.shields.io/github/issues/rafyco/template-python-package.svg
+   :target: https://github.com/rafyco/template-python-package/issues
+
+.. image:: https://img.shields.io/pypi/v/template-python-package.svg
+   :target: https://pypi.python.org/pypi/template-python-package/
+
+.. image:: https://img.shields.io/github/license/rafyco/template-python-package.svg
+   :target: https://www.gnu.org/licenses/gpl.html
+
+
+Description
+-----------
+
+Installation
+~~~~~~~~~~~~
+
+The installation can be carried out in two ways. You can download packages from
+PyPi repository or install it from sources.
+
+For installation from PyPi you can use ``pip`` program. It is likely that you must
+use ``pip3`` instead ``pip``.
+
+::
+
+    pip install template-python-package
+
+You can also use source from github repository to install ``template-python-package``. To make that
+download code and invoke command:
+
+::
+
+    python setup.py install
+
+To checking the installation you can use to call ``template-python-package``. Unittest can be also
+helpfull with verification.
+
+Unit test
+---------
+
+``template-python-package`` have a few unittest that you can use to checking code corectness. You can
+call it from ``setup.py`` file or using ``template-python-package.tests`` module after installation. Check
+one of below command and try it yourselfe.
+
+::
+
+    python setup.py test
+    python -m template-python-package.tests
+
+Author
+------
+
+Rafal Kobel rafalkobel@rafyco.pl
---
+++ setup.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+from __future__ import unicode_literals
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+
+def read_description(module_name):
+    module_doc = __import__(module_name).__doc__.splitlines()
+    result = ""
+    for line in module_doc:
+        if line:
+            result = line
+            break
+    return result
+
+
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 6)
+
+# TODO: change package name
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write("""
+==========================
+Unsupported Python version
+==========================
+
+This version of template-python-package requires Python {}.{}, but you're trying to
+install it on Python {}.{}.
+This may be because you are using a version of pip that doesn't
+understand the python_requires classifier. Make sure you
+have Python {}.{} or newer, then try again:
+
+    $ python3 -m pip install --upgrade pip setuptools
+    $ pip3 install template-python-package 
+
+""".format(*(REQUIRED_PYTHON + CURRENT_PYTHON + REQUIRED_PYTHON)))
+    sys.exit(1)
+
+package_name = 'template-python-package'
+version = __import__(package_name).version
+
+data_files = []
+
+setup(
+    name=package_name,
+    version=version,
+    license='GNU',
+    author="Rafal Kobel",
+    author_email="rafalkobel@rafyco.pl",
+    description=read_description(package_name),
+    python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
+    long_description=open("README.rst").read(),
+    url="https://github.com/rafyco/template-python-package",
+    project_urls={
+        "Source": "https://github.com/rafyco/template-python-package",
+        "Tracker": "https://github.com/rafyco/template-python-package/issues",
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    package_dir={'template-python-package': 'template-python-package'},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ],
+    install_requires=[
+    ],
+    extras_require={
+        "unittests": [
+            "pytest"
+        ],
+        "style": [
+            "pylint == 2.5.3",
+            "pep8 == 1.7.1",
+            "pycodestyle == 2.8.0"
+        ],
+        "typing": [
+            "mypy == 0.910"
+        ],
+        "documentation": [
+            "Sphinx == 4.3.1",
+            "sphinx-epytext == 0.0.4"
+        ]
+    },
+    command_options={
+        'build_spninx': {
+            'project': ('setup.py', package_name),
+            'version': ('setup.py', version),
+            'release': ('setup.py', version),
+            'source_dir': ('setup.py', 'docs')
+        }
+    },
+    platforms="Any",
+    keywords=""
+)
