fdfe8028735d6efb8c3648beed6dc923da6d56f0
Rafał Kobel <rafalkobel@rafyco.pl>
2023-03-29 15:37:54 +0200

    Add a few changes to reduced copied codes

---
+++ ytrss/configuration/readers/file_dict_configuration.py
@@ -0,0 +1,25 @@
+import abc
+import os
+
+from typing import Any
+
+from ytrss.configuration.configuration import ConfigurationError
+from ytrss.configuration.readers.base import ConfigurationReader
+
+
+class FileDictConfiguration(ConfigurationReader, metaclass=abc.ABCMeta):
+    """ Configuration json object """
+
+    @property
+    def read_config(self) -> Any:
+        return self.__configuration
+
+    @abc.abstractmethod
+    def read_data_from_file(self, conf_file: str) -> Any:
+        """ Read data from file """
+
+    def __init__(self, conf_file: str) -> None:
+        if not os.path.isfile(os.path.expanduser(conf_file)):
+            raise ConfigurationError("File not exists")
+
+        self.__configuration = self.read_data_from_file(conf_file)
--- ytrss/core/helpers/logging.py
+++ ytrss/core/helpers/logging.py
@@ -10,46 +10,42 @@ _BOLD_RED = "\x1b[31;1m"
 _RESET = "\x1b[0m"
 
 
-class DebugFormatter(logging.Formatter):
+class BaseFormatter(logging.Formatter):
+    """
+    Default formatter
+    """
+    def __init__(self, message_schema: str) -> None:
+        self._formats: Dict[int, str] = {
+            logging.DEBUG: f"{_BLUE}{message_schema}{_RESET}",
+            logging.INFO: f"{_RESET}{message_schema}{_RESET}",
+            logging.WARNING: f"{_YELLOW}{message_schema}{_RESET}",
+            logging.ERROR: f"{_RED}{message_schema}{_RESET}",
+            logging.CRITICAL: f"{_BOLD_RED}{message_schema}{_RESET}",
+        }
+        super().__init__()
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_fmt = self._formats.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+class DebugFormatter(BaseFormatter):
     """
     Logging formatter for logs in debug mode
     """
 
-    _message_schema = "[%(name)s] %(levelname)s - %(message)s (\"%(pathname)s:%(lineno)d\")"
-
-    _FORMATS: Dict[int, str] = {
-        logging.DEBUG: f"{_BLUE}{_message_schema}{_RESET}",
-        logging.INFO: f"{_RESET}{_message_schema}{_RESET}",
-        logging.WARNING: f"{_YELLOW}{_message_schema}{_RESET}",
-        logging.ERROR: f"{_RED}{_message_schema}{_RESET}",
-        logging.CRITICAL: f"{_BOLD_RED}{_message_schema}{_RESET}",
-    }
-
-    def format(self, record: logging.LogRecord) -> str:
-        log_fmt = self._FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
+    def __init__(self) -> None:
+        super().__init__("[%(name)s] %(levelname)s - %(message)s (\"%(pathname)s:%(lineno)d\")")
 
 
-class ClientFormatter(logging.Formatter):
+class ClientFormatter(BaseFormatter):
     """
     Logging formatter for logs in client app
     """
 
-    _message_schema = "%(message)s"
-
-    _FORMATS: Dict[int, str] = {
-        logging.DEBUG: f"{_BLUE}{_message_schema}{_RESET}",
-        logging.INFO: f"{_RESET}{_message_schema}{_RESET}",
-        logging.WARNING: f"{_YELLOW}{_message_schema}{_RESET}",
-        logging.ERROR: f"{_RED}{_message_schema}{_RESET}",
-        logging.CRITICAL: f"{_BOLD_RED}{_message_schema}{_RESET}",
-    }
-
-    def format(self, record: logging.LogRecord) -> str:
-        log_fmt = self._FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
+    def __init__(self) -> None:
+        super().__init__("%(message)s")
 
 
 logger = logging.getLogger("ytrss")
