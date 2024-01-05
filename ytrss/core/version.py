9ff4ca29d3bedf91e318bf70a6a510c0e6adbd68
GitHub Action <action@github.com>
2024-01-05 09:15:22 +0000

    Upgrade to 0.3.5 version

--- ytrss/core/version.py
+++ ytrss/core/version.py
@@ -4,8 +4,8 @@ class Version:
     def __init__(self) -> None:
         self._major = 0
         self._minor = 3
-        self._patch = 4
-        self._rc = 11
+        self._patch = 5
+        self._rc = 0
 
     @property
     def version(self) -> str:
