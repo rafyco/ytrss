e96a280ff24759a4dd847cfdb39e121bde2d1a99
GitHub Action <action@github.com>
2025-10-23 09:13:49 +0000

    Upgrade to 0.3.5rc22 version

--- ytrss/core/version.py
+++ ytrss/core/version.py
@@ -5,7 +5,7 @@ class Version:
         self._major = 0
         self._minor = 3
         self._patch = 5
-        self._rc = 21
+        self._rc = 22
 
     @property
     def version(self) -> str:
